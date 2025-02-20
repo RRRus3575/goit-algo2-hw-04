c36c8cfe8587e59608576a299dbd1040e32d166f
Ruslan <ruslan.grudistov@gmail.com>
2025-02-20 15:38:55 +0200

    start

---
+++ task_2.py
@@ -0,0 +1,20 @@
+from trie import Trie
+
+class LongestCommonWord(Trie):
+
+    def find_longest_common_word(self, strings) -> str:
+        pass
+
+if __name__ == "__main__":
+    # Тести
+    trie = LongestCommonWord()
+    strings = ["flower", "flow", "flight"]
+    assert trie.find_longest_common_word(strings) == "fl"
+
+    trie = LongestCommonWord()
+    strings = ["interspecies", "interstellar", "interstate"]
+    assert trie.find_longest_common_word(strings) == "inters"
+
+    trie = LongestCommonWord()
+    strings = ["dog", "racecar", "car"]
+    assert trie.find_longest_common_word(strings) == ""
